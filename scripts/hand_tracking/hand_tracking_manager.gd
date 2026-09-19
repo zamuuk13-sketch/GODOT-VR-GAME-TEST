a587531c02e0a97b2f0d2f25dca7519ee7762943
zamuuk13-sketch <zamuuk13@gmail.com>
2026-09-19 13:14:16 -0300

    Auto-start Android hand tracking and retry camera permission

--- scripts/hand_tracking/hand_tracking_manager.gd
+++ scripts/hand_tracking/hand_tracking_manager.gd
@@ -7,10 +7,13 @@ signal camera_status_changed(status: String)
 
 @export var target_fps := 60
 @export var prefer_rear_camera := true
+@export var auto_start_on_android := true
+@export var permission_retry_interval := 0.75
 
 var provider: HandTrackingProvider
 var android_adapter: AndroidHandTrackingAdapter
 var camera_ready := false
+var _permission_retry := 0.0
 
 func _ready() -> void:
 	provider = HandTrackingProvider.new()
@@ -26,10 +29,23 @@ func _ready() -> void:
 	if OS.has_feature("android"):
 		if android_adapter.is_available():
 			camera_status_changed.emit("android_hand_tracking_plugin_ready")
+			if auto_start_on_android:
+				call_deferred("start_tracking")
 		else:
 			camera_status_changed.emit("android_plugin_not_loaded")
 	else:
 		camera_status_changed.emit("desktop_no_android_provider")
+
+func _process(delta: float) -> void:
+	if not OS.has_feature("android") or camera_ready:
+		return
+	if not auto_start_on_android or not android_adapter or not android_adapter.is_available():
+		return
+
+	_permission_retry += delta
+	if _permission_retry >= permission_retry_interval:
+		_permission_retry = 0.0
+		start_tracking()
 
 func start_tracking() -> bool:
 	if android_adapter and android_adapter.is_available():
